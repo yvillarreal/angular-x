f707305966f0c5211ac11c616ddeb3742aeb533f
Yamil <yamil.villarreal@outlook.com>
2020-02-28 17:08:30 -0600

    add module Database

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { HttpClientModule } from "@angular/common/http";
 import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule } from "@angular/router";
 import { ReactiveFormsModule } from "@angular/forms";
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 
 import { AppComponent } from "./app.component";
@@ -19,6 +22,18 @@ import { ShippingComponent } from './shipping/shipping.component';
     BrowserModule,
     HttpClientModule,
     ReactiveFormsModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyCtwywZuCGFqJXanp92B2aKXRqoeHIpDXU",
+      authDomain: "angular-4dc09.firebaseapp.com",
+      databaseURL: "https://angular-4dc09.firebaseio.com",
+      projectId: "angular-4dc09",
+      storageBucket: "angular-4dc09.appspot.com",
+      messagingSenderId: "479962891143",
+      appId: "1:479962891143:web:6adfb7b003bab01f6e4922",
+      measurementId: "G-GR5DG1FF1H"
+    }),
+    AngularFireAuthModule,
+    AngularFirestoreModule.enablePersistence({ synchronizeTabs: true }),
     RouterModule.forRoot([
       { path: "", component: ProductListComponent },
       { path: "products/:productId", component: ProductDetailsComponent },
@@ -38,7 +53,7 @@ import { ShippingComponent } from './shipping/shipping.component';
   bootstrap: [AppComponent],
   providers: [CartService]
 })
-export class AppModule {}
+export class AppModule { }
 
 /*
 Copyright Google LLC. All Rights Reserved.
